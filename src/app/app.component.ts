02d043a1781bed2adabeb02870518cde57fb067a
pulpetto <116948697+pulpetto@users.noreply.github.com>
2023-06-17 22:22:39 +0200

    input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { lorem } from 'faker';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'typing';
+    randomText = lorem.sentence();
+    onInput = function (inputVal: string) {
+        console.log(inputVal);
+    };
 }
